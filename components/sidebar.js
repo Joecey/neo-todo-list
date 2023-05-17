f23ee760f8d82949a36987e6a8eebcbdad45ed38
Joe Linogao <linogaoj@tcd.ie>
2023-05-17 20:54:19 +0100

    finished sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,6 +1,11 @@
 import "../styles/globals.css";
 import "../styles/tailwind.css";
 import Link from "next/link";
+import { IoIosAddCircle } from "react-icons/io";
+import { GoGrabber } from "react-icons/go";
+import { AiOutlineExclamation } from "react-icons/ai";
+import { GoCheck } from "react-icons/go";
+import { BiInfoCircle } from "react-icons/bi";
 
 export default function Sidebar() {
 	return (
@@ -10,15 +15,31 @@ export default function Sidebar() {
             outline-black outline-2 outline items-center z-50 shadow-lg  "
 		>
 			<nav>
-				<SidebarIcon id="new" linkPath="/new" title="new" />
-				<SidebarIcon id="tasks" linkPath="/" title="tasks" />
-				<SidebarIcon id="dueToday" linkPath="/today" title="dueToday" />
 				<SidebarIcon
-					id="completed"
-					linkPath="/complete"
-					title="complete"
+					title="new task"
+					linkPath="/new"
+					icon={<IoIosAddCircle size="32" />}
 				/>
-				<SidebarIcon id="about" linkPath="/about" title="about" />
+				<SidebarIcon
+					title="tasks"
+					linkPath="/"
+					icon={<GoGrabber size="40" />}
+				/>
+				<SidebarIcon
+					title="tasks due"
+					linkPath="/today"
+					icon={<AiOutlineExclamation size="36" />}
+				/>
+				<SidebarIcon
+					title="completed"
+					linkPath="/complete"
+					icon={<GoCheck size="32" />}
+				/>
+				<SidebarIcon
+					title="About"
+					linkPath="/about"
+					icon={<BiInfoCircle size="32" />}
+				/>
 			</nav>
 		</div>
 	);
@@ -26,8 +47,12 @@ export default function Sidebar() {
 
 function SidebarIcon(props) {
 	return (
-		<Link href={props.linkPath} className="side-icon ">
-			<h1>{props.title}</h1>
+		<Link href={props.linkPath} className="side-icon group">
+			{props.icon}
+
+			<span className="side-icon-tooltip task-title shadow-solid-secondary group-hover:scale-100">
+				{props.title}
+			</span>
 		</Link>
 	);
 }
